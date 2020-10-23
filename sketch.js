230c82ea70165920ac152ae03789c9bc5e279ac6
Cara-Cai <54806132+Cara-Cai@users.noreply.github.com>
2020-10-23 12:57:47 +0800

    change the direction

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ let balls=[];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ball0 = {
-    x: random(50),
+    x: random(width-50),
     y: random(height),
     dy: 0,
   }
   ball1 = {
-    x: random(50),
+    x: random(width-50),
     y: random(height),
     dy: 0,
   }
@@ -19,17 +19,17 @@ function setup() {
 function draw() {
   background(200);
   ball0.x += ball0.dy;
-  ball0.dy += 0.25;
+  ball0.dy += -0.25;
   circle(ball0.x, ball0.y, 50);
   ball1.x += ball1.dy;
-  ball1.dy += 0.25;
+  ball1.dy += -0.25;
   circle(ball1.x, ball1.y, 50);
   // if the object hits the bottom of the canvas
   // make it go up instead of down
-  if (ball0.x >= width) {
+  if (ball0.x <= 0) {
     ball0.dy = -0.95 * ball0.dy;
   }
-  if (ball1.x >= width) {
+  if (ball1.x<= 0) {
     ball1.dy = -0.95 * ball1.dy;
   }
 }
