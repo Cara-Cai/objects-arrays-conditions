7a94f64af74d515af75fd14b51acb3bc4cad6675
Cara-Cai <54806132+Cara-Cai@users.noreply.github.com>
2020-10-23 12:54:28 +0800

    horizontally bump

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ let balls=[];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ball0 = {
-    x: random(width),
-    y: random(50),
+    x: random(50),
+    y: random(height),
     dy: 0,
   }
   ball1 = {
-    x: random(width),
-    y: random(50),
+    x: random(50),
+    y: random(height),
     dy: 0,
   }
   balls.push[ball0];
@@ -18,18 +18,18 @@ function setup() {
 }
 function draw() {
   background(200);
-  ball0.y += ball0.dy;
+  ball0.x += ball0.dy;
   ball0.dy += 0.25;
   circle(ball0.x, ball0.y, 50);
-  ball1.y += ball1.dy;
+  ball1.x += ball1.dy;
   ball1.dy += 0.25;
   circle(ball1.x, ball1.y, 50);
   // if the object hits the bottom of the canvas
   // make it go up instead of down
-  if (ball0.y >= height) {
+  if (ball0.x >= width) {
     ball0.dy = -0.95 * ball0.dy;
   }
-  if (ball1.y >= height) {
+  if (ball1.x >= width) {
     ball1.dy = -0.95 * ball1.dy;
   }
 }
